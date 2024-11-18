60e7de4d9b10a4a9674d7cf828309e4c649350fc
Irina Bagut <irina.bagut@msg.group>
2024-11-19 01:13:59 +0200

    Angular cli 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FioriSAPDemo';
+  tableRows: any[] | undefined;
+
+  ngOnInit(): void {
+    this.tableRows = [
+        {
+            column1: 'user.name@email.com',
+            column2:
+                'LongText LongText LongText LongText LongText LongText LongText LongText LongText LongText LongText',
+            column3: 'Row 2',
+            date: '09-07-18',
+            type: 'search'
+        },
+        {
+            column1: 'user.name@email.com',
+            column2: 'Row 2',
+            column3:
+                'Wrapped Long Text Wrapped Long Text Wrapped Long Text Wrapped Long Text Wrapped Long Text Wrapped Long',
+            date: '09-08-18',
+            type: 'cart'
+        },
+        {
+            column1: 'user.name@email.com',
+            column2: 'Row 3',
+            column3: 'Row 3',
+            date: '02-14-18',
+            type: 'calendar'
+        },
+        {
+            column1: 'user.name@email.com',
+            column2: 'Row 4',
+            column3: 'Row 4',
+            date: '12-30-17',
+            type: 'search'
+        },
+        {
+            column1: 'user.name@email.com',
+            column2: 'Row 5',
+            column3: 'Row 5',
+            date: '11-12-18',
+            type: 'search'
+        }
+    ];
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-//import { ButtonModule } from '@fundamental-ngx/core';
+import { FundamentalNgxCoreModule } from '@fundamental-ngx/core';
+import { ButtonModule } from '@fundamental-ngx/core/button';
+
+
 
 @NgModule({
   declarations: [
@@ -13,7 +17,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    BrowserAnimationsModule,
+    FundamentalNgxCoreModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
