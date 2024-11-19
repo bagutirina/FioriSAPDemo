ef8e8ccbca9dba2d7ddfd33685a230e5af53f878
Irina Bagut <irina.bagut@msg.group>
2024-11-19 03:14:59 +0200

    yes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule, FundamentalNgxCoreModule } from '@fundamental-ngx/core';
 
-import { FundamentalNgxCoreModule } from '@fundamental-ngx/core';
-import { ButtonModule } from '@fundamental-ngx/core/button';
+
 
 
 
