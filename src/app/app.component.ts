043967a86dca3fba6a3c7c7cf6822edf6f6500b7
Ir19a136 <bagutirina@yahoo.com>
2025-05-20 23:02:31 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Fiori SAP Demo';
+  title = '';
 
   constructor() {}
 }
